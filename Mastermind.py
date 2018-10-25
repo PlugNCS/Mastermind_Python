f09587570b712908f37d0b9eea54f8babbe8dc39
Rémi Gomez <remi.gomez@live.fr>
2018-10-25 18:19:16 +0200

    v

--- Mastermind.py
+++ Mastermind.py
@@ -1,5 +1,9 @@
 # Mastermind in Python
 # Version 1.0
 # (c) 2018 Michaël NASS & Remi GOMEZ
+name = input("Hello player, what's your name ?")
+color = [20]
+color[0] = ""
+print("\e[196;11;13m test")
 
 
