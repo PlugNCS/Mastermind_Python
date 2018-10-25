16ff1ee25de88e40c177de8aa174a2cd84b0d831
Rémi Gomez <remi.gomez@live.fr>
2018-10-25 19:46:19 +0200

    v

--- Mastermind.py
+++ Mastermind.py
@@ -2,6 +2,7 @@
 # Version 1.0
 # (c) 2018 Michaël NASS & Remi GOMEZ
 import random
+import time
 name = input("Hello player, what's your name? ")
 colors = int(input("How many colours do you want? "))
 size = int(input("How long should the size of the code be? "))
@@ -11,7 +12,24 @@ i = 0
 while i < size - 1:
     code.append(random.randint(0, colors))
     i += 1
-
+count = 0 
+t0 = time.time()
+pepegs = 0
+win = False
+while count < attempts and win == False:
+    count += 1
+    gcode = input("Attempt number ", count, ": Enter the code: ")
+    acode = gcode.split()
+    cnt = 0
+    while cnt < size:
+        if acode[cnt]==code[cnt]:
+            pepegs += 1
+        cnt += 1
+    if pepegs == size:
+        win = True
+            
+t1 = time.time()
+total = t1 - t0
 
 
 
